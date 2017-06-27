30dfb157e06eef1f77ad4a420e405322aa109980
Markus Hiesmair <markus.hiesmair@yahoo.de>
2017-06-27 13:49:38 +0200

    asdf

--- app.py
+++ app.py
@@ -5,16 +5,16 @@ import time
 
 
 def p(sensor_name, timestamp, sensed_values):
-    print(sensor_name)
-    print(sensed_values)
+    print(timestamp, sensor_name, sensed_values)
 
 sensor_file_writer = SensorFileWriter('/home/pi/Desktop/data/raw_' + str(time.time()) + '.dat')
 
-lidar_poller = LidarLitePoller(observers=p, sensing_interval_in_s=1)
-gps_poller = GpsPoller(observers=[p, sensor_file_writer.writeLine])
+lidar_poller = LidarLitePoller(observers=p, sensing_interval_in_s=0.1)
+#gps_poller = GpsPoller(observers=[p, sensor_file_writer.writeLine])
+gps_poller = GpsPoller(observers=[p])
 try:
     gps_poller.start()
-    # lidar_poller.start()
+    lidar_poller.start()
     while True:
         time.sleep(1)
 
@@ -24,5 +24,5 @@ except (KeyboardInterrupt, SystemExit):  # when you press ctrl+c
 gps_poller.running = False
 gps_poller.join(timeout=5)
 
-#lidar_poller.running = False
-#lidar_poller.join(timeout=5)
+lidar_poller.running = False
+lidar_poller.join(timeout=5)
